f277e13e37c5d7545fb07edaa90e96db80041c1f
connectayan27 <connect.ayan27@gmail.com>
2026-09-05 10:37:24 +0530

    Add files via upload
    Range and structure

---
+++ range.py
@@ -0,0 +1,23 @@
+n = 10
+a, b = 0, 1
+
+for i in range(n):
+    print(a, end=" ")
+    next_val = a + b
+    a, b = b, next_val
+
+
+
+class Student:
+    def __init__(self, name, roll, marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+
+# Create a Student object
+s1 = Student("Rahul", 101, 87.5)
+
+# Print details
+print("Name:", s1.name)
+print("Roll:", s1.roll)
+print("Marks:", s1.marks)
